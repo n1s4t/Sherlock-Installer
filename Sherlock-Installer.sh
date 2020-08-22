444c7b6f8549c6996ed23a4ca2d5c83575dbc52c
Tasin-coder <61790940+Tasin-coder@users.noreply.github.com>
2020-08-22 17:49:24 +0600

    Update Sherlock-Installer.sh

--- Sherlock-Installer.sh
+++ Sherlock-Installer.sh
@@ -1,21 +1,25 @@
 cd 
 clear
-echo -e "\033[93mPkg Update & Upgrading"
+echo -e '033[92m'
+echo "  ___           _        _ _ _   "
+echo " |_ _|_ __  ___| |_ __ _| | (_)_ __   __ _"
+echo "  | || '_ \/ __| __/ _` | | | | '_ \ / _` |"
+echo "  | || | | \__ \ || (_| | | | | | | | (_| |"
+echo " |___|_| |_|___/\__\__,_|_|_|_|_| |_|\__, |"
+echo "                                     |___/ "
+
+echo -e '\033[93mPkg update and upgrading'
 pkg update && upgrade
 
-echo -e "\033[92mInstalling Requirements"
-pkg install git && python2 && python3
-
-echo -e "\033[92mCloning Sherlock"
+echo -e "\033[93mCloning Sherlock"
 git clone https://github.com/sherlock-project/sherlock.git
 
-echo -e "\033[92mWorking"
-pkg install python2 pip
-
+echo -e "\033[93mWorking"
+pkg install python2 pip -y
 cd
 
-echo -e "Chainging Dirrectry"
-cd sherlock
+echo -e "\033[93mChainging Dirrectry"
+cd Sherlock
 
-echo -e "\033[91mInstalling Requirements"
+echo -e "\033[93mInstalling Requirements"
 python3 -m pip install -r requirements.txt
